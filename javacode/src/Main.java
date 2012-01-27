90f081703f524ae04d37f272bbbbb7432f041e82
Peter Boothe <pboothe@gmail.com>
2012-01-27 01:03:40 -0500

    Adding things.  Hopefully this will lay well with Eclipse.

---
+++ javacode/src/Main.java
@@ -0,0 +1,13 @@
+import java.awt.Frame;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Frame f = new Frame("Evolving Prisoner's Dilemma Agents");
+		
+		f.pack();
+		f.setVisible(true);
+	}
+
+}
